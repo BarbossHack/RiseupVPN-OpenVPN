a7dbe6f0f8733514b5796f847bb505c690158582
BarbossHack <barbossh4ck@protonmail.com>
2023-12-27 17:43:39 +0100

    Retry curl if failed (slow api servers)

--- generate.sh
+++ generate.sh
@@ -5,8 +5,10 @@ set -u
 
 OVPN_CONF=riseup-ovpn.conf
 
+echo "[+] Please wait, riseup API is slow..."
+
 # Download new VPN client certs (private and public keys)
-curl -s --cacert riseup-ca.crt https://api.black.riseup.net/3/cert -o riseup-vpn.pem
+curl -s --connect-timeout 5 --retry 5 --cacert riseup-ca.crt https://api.black.riseup.net/3/cert -o riseup-vpn.pem
 chmod 0600 riseup-vpn.pem
 
 # Copy the sample openvpn conf
@@ -14,7 +16,7 @@ cp riseup-ovpn.sample.conf $OVPN_CONF
 sed -i 's/^remote .*$//g' $OVPN_CONF
 
 # Get the VPN IP list, and add them to openvpn conf
-IP_LIST=$(curl -s --cacert riseup-ca.crt https://api.black.riseup.net/3/config/eip-service.json | jq -r .gateways[].ip_address)
+IP_LIST=$(curl -s --connect-timeout 5 --retry 5 --cacert riseup-ca.crt https://api.black.riseup.net/3/config/eip-service.json | jq -r .gateways[].ip_address)
 for ip in $IP_LIST; do
     sed -i "/^remote-random$/i remote $ip 1194" $OVPN_CONF
 done
