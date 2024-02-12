d5ced3729c857789f11c623b7e635fd211e49685
BarbossHack <barbossh4ck@protonmail.com>
2024-02-12 21:44:58 +0100

    use all available ports

--- generate.sh
+++ generate.sh
@@ -22,9 +22,18 @@ cp riseup-ovpn.sample.conf $OVPN_CONF
 sed -i 's/^remote .*$//g' $OVPN_CONF
 
 # Get the VPN IP list, and add them to openvpn conf
-IP_LIST=$(curl ${DEBUG:+-v} -s --connect-timeout 5 --retry 5 https://api.black.riseup.net/3/config/eip-service.json | jq -r .gateways[].ip_address)
-for ip in $IP_LIST; do
-    sed -i "/^remote-random$/i remote $ip 1194" $OVPN_CONF
+gateways=$(curl ${DEBUG:+-v} -s --connect-timeout 5 --retry 5 https://api.black.riseup.net/3/config/eip-service.json | jq '.gateways')
+for gateway_b64 in $(echo "$gateways" | jq -r '.[] | @base64'); do
+
+    gateway=$(echo $gateway_b64 | base64 --decode)
+    ip_address=$(echo $gateway | jq -r '.ip_address')
+    host=$(echo $gateway | jq -r '.host')
+    location=$(echo $gateway | jq -r '.location')
+    ports=$(echo $gateway | jq -r '.capabilities.transport[] | select( .type | contains("openvpn")) | .ports[]')
+
+    for port in $ports; do
+        sed -i "/^remote-random$/i remote $ip_address $port # $host ($location)" $OVPN_CONF
+    done
 done
 
 echo "[+] OpenVPN conf was created with success, you can now run :"
