5f3effb328c59c764701f5a8e7ac113351db9310
BarbossHack <barbossh4ck@protonmail.com>
2024-10-21 21:00:32 +0200

    add a verbose verbose mode (-vvv)

--- generate.sh
+++ generate.sh
@@ -4,17 +4,20 @@ set -eu
 
 # Verbose mode
 if [ $# == 1 ] && [ "$1" == "-v" ]; then
-    VERBOSE=1
-    NO_SILENT=
+    CURL_NO_SILENT=
+elif [ $# == 1 ] && [ "$1" == "-vvv" ]; then
+    CURL_VERBOSE=1
+    CURL_NO_SILENT=
     set -x
 fi
 
 OVPN_CONF=riseup-ovpn.conf
 
-echo -e "\e[36mPlease wait, riseup API can be very slow...\e[0m"
+echo "Please wait, riseup API can be very slow..."
 
 # Download new VPN client certs (private and public keys)
-key_cert=$(curl ${VERBOSE:+-v} ${NO_SILENT--sS} --fail --connect-timeout 10 --retry 3 -H "Accept: text/html" https://api.black.riseup.net/3/cert)
+if [ ! -z ${CURL_NO_SILENT+x} ]; then echo "curl riseup certificate from https://api.black.riseup.net/3/cert"; fi
+key_cert=$(curl ${CURL_VERBOSE:+-v} ${CURL_NO_SILENT--sS} --fail --connect-timeout 10 --retry 3 -H "Accept: text/html" https://api.black.riseup.net/3/cert)
 
 # Copy the sample openvpn conf
 cp riseup-ovpn.sample.conf $OVPN_CONF
@@ -23,7 +26,8 @@ echo -e "\n<key>\n$key_cert\n</key>" >>$OVPN_CONF
 echo -e "\n<cert>\n$key_cert\n</cert>" >>$OVPN_CONF
 
 # Get the VPN IP list, and add them to openvpn conf
-gateways=$(curl ${VERBOSE:+-v} ${NO_SILENT--sS} --fail --connect-timeout 10 --retry 3 -H "Accept: application/json" https://api.black.riseup.net/3/config/eip-service.json | jq '.gateways')
+if [ ! -z ${CURL_NO_SILENT+x} ]; then echo "curl riseup servers list from https://api.black.riseup.net/3/config/eip-service.json"; fi
+gateways=$(curl ${CURL_VERBOSE:+-v} ${CURL_NO_SILENT--sS} --fail --connect-timeout 10 --retry 3 -H "Accept: application/json" https://api.black.riseup.net/3/config/eip-service.json | jq '.gateways')
 
 for gateway_b64 in $(echo "$gateways" | jq -r '.[] | @base64'); do
     gateway=$(echo $gateway_b64 | base64 --decode)
@@ -36,5 +40,5 @@ for gateway_b64 in $(echo "$gateways" | jq -r '.[] | @base64'); do
     done
 done
 
-echo "[+] OpenVPN conf was created with success !"
-echo -e "\e[36msudo openvpn --config $OVPN_CONF\e[0m"
+echo "OpenVPN conf was created with success !"
+echo -e "\e[36m$ sudo openvpn --config $OVPN_CONF\e[0m"
