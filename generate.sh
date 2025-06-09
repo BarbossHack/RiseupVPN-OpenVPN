c1864286815296f4dbf8138b351bcd7b53daddd8
BarbossHack <barbossh4ck@protonmail.com>
2025-06-09 21:10:58 +0200

    chore: move requirement checks and improve script compatibility

--- generate.sh
+++ generate.sh
@@ -1,13 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -eu
-
-if ! command -v jq >/dev/null 2>&1; then
-    echo "Please install jq to run this script."
-    echo "  $ sudo apt install jq"
-    echo "  $ sudo dnf install jq"
-    exit 1
-fi
 
 ARGS=" $* "
 VERB=1
@@ -22,6 +15,17 @@ elif [[ "$ARGS" = *" -vvv "* ]]; then
 fi
 
 OVPN_CONF=riseup-ovpn.conf
+
+# Check requirements
+if ! command -v jq >/dev/null 2>&1 || ! command -v curl >/dev/null 2>&1 || ! command -v ip >/dev/null 2>&1; then
+    echo -e "\e[31m[-] Please install the following requirements to run this script:\e[0m"
+    echo "Debian    $ apt install -y jq curl iproute2"
+    echo "Fedora    $ dnf install -y jq curl iproute2"
+    echo "Alpine    $ apk add jq curl iproute2"
+    echo "Nixos     $ nix-shell -p jq curl iproute2"
+    echo "Archlinux $ pacman -Sy --noconfirm jq curl iproute2"
+    exit 1
+fi
 
 echo "Please wait, riseup API can be very slow..."
 
@@ -48,7 +52,7 @@ pull-filter ignore "ifconfig-ipv6"
 pull-filter ignore "redirect-gateway"
 block-ipv6
 redirect-gateway def1' >>$OVPN_CONF
-elif ! ip a | grep inet6; then
+elif ! ip a | grep inet6 >/dev/null 2>&1; then
     echo -e "\e[33;3mIPv6 appears to be disabled on your host. You may want to explicitly disable it using --no-ipv6\e[0m"
 fi
 
@@ -58,7 +62,7 @@ if [ -n "${CURL_NO_SILENT+x}" ]; then echo "curl riseup servers list from https:
 gateways=$(curl ${CURL_VERBOSE:+-v} ${CURL_NO_SILENT--sS} --fail --connect-timeout 10 --retry 3 -H "Accept: application/json" https://api.black.riseup.net/3/config/eip-service.json | jq '.gateways')
 
 for gateway_b64 in $(echo "$gateways" | jq -r '.[] | @base64'); do
-    gateway=$(echo "$gateway_b64" | base64 --decode)
+    gateway=$(echo "$gateway_b64" | base64 -d)
     ip_address=$(echo "$gateway" | jq -r '.ip_address')
     host=$(echo "$gateway" | jq -r '.host')
     location=$(echo "$gateway" | jq -r '.location')
